44385d00b505f6507e9ea5af1bc204a0a74aace2
Jacob Parker <jacobparker1992@gmail.com>
2016-12-08 14:35:45 +0000

    Update package json

--- babel-plugin-cssta/.eslintrc.js
+++ babel-plugin-cssta/.eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    "extends": "airbnb",
+    "extends": "airbnb-base",
+    "installedESLint": true,
     "plugins": [
-        "react",
-        "jsx-a11y",
         "import"
-    ]
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [0],
+        "comma-dangle": ["error", {
+            arrays: "always-multiline",
+            objects: "always-multiline",
+            imports: "always-multiline",
+            exports: "always-multiline",
+            functions: "never",
+        }],
+    }
 };
--- cssta/.eslintrc.js
+++ cssta/.eslintrc.js
@@ -2,8 +2,6 @@ module.exports = {
     "extends": "airbnb-base",
     "installedESLint": true,
     "plugins": [
-        "react",
-        "jsx-a11y",
         "import"
     ],
     "rules": {
