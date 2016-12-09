ebe61088ef2c9ee3fc6851a8ff3b051748c420f4
Jacob Parker <jacobparker1992@gmail.com>
2016-12-09 12:55:52 +0000

    Further code cleanup

--- babel-plugin-cssta/src/optimizations/removeSetPostCssPipeline.js
+++ babel-plugin-cssta/src/optimizations/removeSetPostCssPipeline.js
@@ -10,14 +10,22 @@ module.exports = (element, state, node) => {
     throw new Error('Expected argument to setPostCssPipeline to be an array');
   }
 
-  const localNames = _.flow(
+  const bindingIdentifiers = _.flow(
     _.filter(t.isCallExpression),
-    _.map('callee.name')
+    _.map('callee.name'),
+    _.map(_.propertyOf(element.scope.bindings)),
+    _.filter({ references: 1 }),
+    _.map('path')
   )(arg.elements);
 
-  _.forEach((localName) => {
-    removeReference(state, localName);
-  }, localNames);
+  _.forEach((importSpecifier) => {
+    const importDeclaration = importSpecifier.findParent(t.isImportDeclaration);
+    if (importDeclaration.node.specifiers.length === 1) {
+      importDeclaration.remove();
+    } else {
+      importSpecifier.remove();
+    }
+  }, bindingIdentifiers);
 
   removeReference(state, callee.object.name);
   element.remove();
