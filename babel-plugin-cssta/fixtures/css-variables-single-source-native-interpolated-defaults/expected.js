7cf194921bbc3bf5f73153ac746d8a336b6f1995
Jacob Parker <jacobparker1992@gmail.com>
2017-01-24 12:28:13 +0000

    Add test

---
+++ babel-plugin-cssta/fixtures/css-variables-single-source-native-interpolated-defaults/expected.js
@@ -0,0 +1,21 @@
+import { StyleSheet as _StyleSheet } from 'react-native';
+import _staticComponent from 'cssta/dist/native/staticComponent';
+
+import { View } from 'react-native';
+
+const color = 'blue';
+
+_staticComponent(View, [], []);
+
+var _csstaStyle2 = _StyleSheet.create({
+  0: {
+    'color': 'red'
+  }
+});
+
+_staticComponent(View, [], [{
+  'validate': function (p) {
+    return true;
+  },
+  'style': _csstaStyle2[0]
+}]);
