b359800fafbabab50104d318b6565ac68cad8c76
Jacob Parker <jacobparker1992@gmail.com>
2017-03-06 20:20:12 +0000

    Remove compiled files

--- packages/cssta/lib/native/dynamicComponent.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var dynamicComponentFactory = require('../factories/dynamicComponentFactory');
-var staticComponentTransform = require('./staticComponentTransform');
-
-module.exports = dynamicComponentFactory(function (ownProps, passedProps, args) {
-  return staticComponentTransform(ownProps, passedProps, args.rules);
-});
--- packages/cssta/lib/native/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports.getAppliedRules = function (rules, ownProps) {
-  return rules.filter(function (rule) {
-    return rule.validate ? rule.validate(ownProps) : true;
-  });
-};
