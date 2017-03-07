6cb6462864db5ba7bf15f20ab2c398f7cfd102e4
Jacob Parker <jacobparker1992@gmail.com>
2017-03-07 09:39:44 +0000

    Remove util file only used in one file

--- packages/cssta/src/factories/componentFactory.js
+++ packages/cssta/src/factories/componentFactory.js
@@ -1,7 +1,51 @@
 /* eslint-disable no-param-reassign */
 const React = require('react');
-const { getOwnPropKeys, getComponentProps, getPropTypes } = require('../util/props');
 
+const { PropTypes } = React;
+
+const getOwnPropKeys = propTypes =>
+  (Array.isArray(propTypes) ? propTypes : Object.keys(propTypes));
+
+const getComponentProps = (ownPropKeys, component, props) => (
+  Object.keys(props).reduce((accum, key) => {
+    const prop = props[key];
+
+    if (key === 'component') {
+      accum.Element = prop;
+    } else if (ownPropKeys.indexOf(key) !== -1) {
+      accum.ownProps[key] = prop;
+    } else {
+      accum.passedProps[key] = prop;
+    }
+
+    return accum;
+  }, {
+    Element: component,
+    ownProps: {},
+    passedProps: {},
+  })
+);
+
+let getPropTypes;
+if (process.env.NODE_ENV !== 'production') {
+  getPropTypes = (ownPropKeys, propTypes) => ownPropKeys.reduce((out, key) => {
+    const styleMap = propTypes[key];
+    const propType = styleMap.type;
+
+    if (propType === 'oneOf') {
+      out[key] = PropTypes.oneOf(styleMap.values);
+    } else if (propType === 'bool') {
+      out[key] = PropTypes.bool;
+    }
+
+    return out;
+  }, {
+    component: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.function,
+    ]),
+  });
+}
 
 module.exports = (transformProps) => {
   const baseRender = ({ Element, ownProps, passedProps, args }) =>
