9b49ecae414474c30487817043db08fa2ea0c89d
Jacob Parker <jacobparker1992@gmail.com>
2018-03-20 19:27:00 +0000

    Rename media queries

--- src/native/enhancers/__tests__/resolveVariableDependencies.js
+++ src/native/enhancers/__tests__/resolveVariableDependencies.js
@@ -1,24 +1,15 @@
 const resolveVariableDependencies = require("../resolveVariableDependencies");
 
 it("should resolve a single variable", () => {
-  const actualVariables = resolveVariableDependencies(
-    {
-      color: "red"
-    },
-    {}
-  );
+  const actualVariables = resolveVariableDependencies({ color: "red" }, {});
 
   expect(actualVariables).toEqual({ color: "red" });
 });
 
 it("should resolve a variable refercing a higher scope", () => {
   const actualVariables = resolveVariableDependencies(
-    {
-      color: "var(--primary)"
-    },
-    {
-      primary: "red"
-    }
+    { color: "var(--primary)" },
+    { primary: "red" }
   );
 
   expect(actualVariables).toEqual({ color: "red" });
@@ -26,56 +17,35 @@ it("should resolve a variable refercing a higher scope", () => {
 
 it("should resolve a variable refercing the current scope", () => {
   const actualVariables = resolveVariableDependencies(
-    {
-      color: "var(--primary)",
-      primary: "red"
-    },
+    { color: "var(--primary)", primary: "red" },
     {}
   );
 
-  expect(actualVariables).toEqual({
-    color: "red",
-    primary: "red"
-  });
+  expect(actualVariables).toEqual({ color: "red", primary: "red" });
 });
 
 it("have the current scope override higher scopes", () => {
   const actualVariables = resolveVariableDependencies(
-    {
-      color: "var(--primary)",
-      primary: "red"
-    },
-    {
-      primary: "blue"
-    }
+    { color: "var(--primary)", primary: "red" },
+    { primary: "blue" }
   );
 
-  expect(actualVariables).toEqual({
-    color: "red",
-    primary: "red"
-  });
+  expect(actualVariables).toEqual({ color: "red", primary: "red" });
 });
 
 it("should allow defaults", () => {
   const actualVariables = resolveVariableDependencies(
-    {
-      color: "var(--primary, blue)"
-    },
+    { color: "var(--primary, blue)" },
     {}
   );
 
-  expect(actualVariables).toEqual({
-    color: "blue"
-  });
+  expect(actualVariables).toEqual({ color: "blue" });
 });
 
 it("should not allow circular dependencies", () => {
   expect(() => {
     resolveVariableDependencies(
-      {
-        color: "var(--primary)",
-        primary: "var(--color)"
-      },
+      { color: "var(--primary)", primary: "var(--color)" },
       {}
     );
   }).toThrow();
